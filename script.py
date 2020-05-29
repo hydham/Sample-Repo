1445ca831ca697a81837558f41e8458cd40394ff
Hydham Abdul Rahman <hydham007@gmail.com>
2020-05-29 16:29:42 +0530

    Removed some code

--- script.py
+++ script.py
@@ -1,16 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://monhapp.com")
 print(r.status_code)
+print(r.ok)
